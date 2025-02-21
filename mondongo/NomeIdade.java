b67ff34b205b1d794076d6947442f7120ae324d8
Daniellasalle1 <daniel.oliveia@soulasalle.com.br>
2025-02-20 21:27:02 -0300

    Add files via upload

---
+++ mondongo/NomeIdade.java
@@ -0,0 +1,127 @@
+package mondongo;
+
+import java.util.Scanner;
+
+public class NomeIdade {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+
+    	
+    	//1
+       /* System.out.print("escreva um numero: ");
+        double n1 = sc.nextInt();
+        System.out.print("escreva outro numero: ");
+        double n2 = sc.nextInt();
+        
+        double total = n1 + n2;
+        
+        System.out.print("o total é  " + total);*/
+    	
+    	//2
+    	
+    	/*System.out.print("escreva o raio do circulo: ");
+    	double ray = sc.nextInt();
+    	
+    	double total = 3.14159 * (ray * ray);
+    	
+    	System.out.println("o total é: " + total);*/
+    	
+    	//3
+    	
+    	/*System.out.print("escreva uma temperatura: ");
+    	double temp = sc.nextInt();
+    	
+    	double f = (temp * 9/5) + 32;
+    	
+    	System.out.println("o valor em farenheight é: " + f);*/
+    	
+    	//4 
+    	
+    	/*System.out.print("escreva um numero: ");
+        double n1 = sc.nextInt();
+        System.out.print("escreva um numero: ");
+        double n2 = sc.nextInt();
+        System.out.print("escreva um numero: ");
+        double n3 = sc.nextInt();
+        
+        double total = (n1 + n2 + n3) / 3;
+    	
+        
+        System.out.println("a media é: " + total);*/
+    	
+    	
+    	//5
+    	
+    	/*System.out.print("escreva sua idade: ");
+    	double idade = sc.nextInt();
+    	
+    	double total = idade * 365;
+    	
+    	System.out.println("o total de dias é: " + total);
+    	*/
+    	
+    	//6
+    	
+    	/*System.out.print("escreva seu salario bruto: ");
+    	double salario = sc.nextInt();
+    	System.out.print("escreva seu desconto inss: ");
+    	double desconto = sc.nextInt();
+    	
+    	System.out.println("o desconto é:" + (salario - desconto));*/
+    	
+    	
+    	//7
+    	
+    	/*System.out.print("escreva a base do retangulo: ");
+    	double base = sc.nextInt();
+    	System.out.print("escreva a altura: ");
+    	double altura = sc.nextInt();
+    	
+    	System.out.println("a area é: " + (base * altura));
+    	System.out.println("o perimetro é: " + (base * altura) * 2);*/
+    	
+    	//8
+    	
+    	/*System.out.print("escreva o raio: ");
+    	double raio = sc.nextInt();
+    	
+    	double volume = (4/3) * 3.14159 * (raio * raio * raio);
+    	
+    	System.out.print("o volume é " + volume);*/
+    	
+    	//9
+    	
+    	/*System.out.print("escreva o valor em real: ");
+    	double dinheiro = sc.nextInt();
+    	
+    	System.out.println("o valor em dolar é: " + (dinheiro * 5.70));*/
+    	
+    	//10
+    	
+    	System.out.print("escreva o valor de x1: ");
+    	double x1 = sc.nextInt();
+    	System.out.print("escreva o valor de x2: ");
+    	double x2 = sc.nextInt();
+    	System.out.print("escreva o valor de y1: ");
+    	double y1 = sc.nextInt();
+    	System.out.print("escreva o valor de y2: ");
+    	double y2 = sc.nextInt();
+    	
+    	
+    	double total = Math.sqrt((x2 - x2) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    	
+    	System.out.println("o total é: " + total);
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    }
+}
